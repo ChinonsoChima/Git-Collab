cb786e1c8545e2495bf066b6a446b187ceebc3a9
Chinonso Anyanka <cc792@njit.edu>
2021-01-31 10:35:53 -0500

    Feature: added definitions for the terms repository, clone, and fork

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 #Hello Github
+
+#Repository- directory or storage space either on your computer or on Github where you can store your files
+
+#clone-making a copy of a repository on Github onto your local computer
+
+#fork-creating a personal copy of someone else's project
+
+#
+
+#
+
+#
+
+#
+
+#
